bcbe12f32e7d1b537147055e521aa6c19c00fa71
Alden Brown <alden.brown@cirium.com>
2020-03-28 13:42:50 -0700

    Issue #24 - Allow for specifying transition time and easing curve
    Added duration and easing props
    Set autoSlide to autoSlide + duration to account for the animation timing
    Added the duration and easing props the root.js story for demonstration purposes

--- src/components/SliderList.js
+++ src/components/SliderList.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 const SliderList = styled.div`
-  transform: translateX(${({ translateX }) => translateX ? `-${translateX}%` : '0%'});
-  transition: transform 0.6s ease-in-out;
+  transform: translateX(
+    ${({ translateX }) => (translateX ? `-${translateX}%` : '0%')}
+  );
+  transition: transform
+    ${({ duration }) => (duration ? `${duration}ms` : '600ms')}
+    ${({ easing }) => easing || 'ease-in-out'};
 `;
+
 export default SliderList;
